6d2578d308b27d9d1ef8b270737550a9a7956d91
hudy9x <hudy9x@gmail.com>
2023-09-29 11:10:43 +0700

    update README.md

--- demos/nextjs13/src/app/page.js
+++ demos/nextjs13/src/app/page.js
@@ -1,36 +1,18 @@
 import Image from "next/image";
 
 {/*R_IMPORT_START*/}
-          import HeroSection10 from '@/components/HeroSection10'
-          import LogoClouds5 from '@/components/LogoClouds5'
-          import FeatureSection4 from '@/components/FeatureSection4'
-          import FeatureSection12 from '@/components/FeatureSection12'
           import FeatureSection7 from '@/components/FeatureSection7'
-          import FeatureSection2 from '@/components/FeatureSection2'
-          import CTASection1 from '@/components/CTASection1'
-          import Testimonials8 from '@/components/Testimonials8'
-          import PricingSection13 from '@/components/PricingSection13'
-          import FAQs10 from '@/components/FAQs10'
-          import ContactSection3 from '@/components/ContactSection3'
-          import Footers8 from '@/components/Footers8'
+          import LogoClouds10 from '@/components/LogoClouds10'
+          import ContentSection2 from '@/components/ContentSection2'
           {/*R_IMPORT_END*/}
 
 export default function Home() {
   return <>
 
 {/*R_CONTENT_START*/}
-          <HeroSection10 />
-          <LogoClouds5 />
-          <FeatureSection4 />
-          <FeatureSection12 />
           <FeatureSection7 />
-          <FeatureSection2 />
-          <CTASection1 />
-          <Testimonials8 />
-          <PricingSection13 />
-          <FAQs10 />
-          <ContactSection3 />
-          <Footers8 />
+          <LogoClouds10 />
+          <ContentSection2 />
           {/*R_CONTENT_END*/}
     
     </>;
